db576c529be1b5d7a8b31a86c5c92419ee11f8c1
aladinez <informaticienalae@gmail.com>
2021-11-27 17:26:24 +0100

    added listing func in book class

--- Module_00/ex01/Phonebook.cpp
+++ Module_00/ex01/Phonebook.cpp
@@ -3,21 +3,54 @@
 int main()
 {
     Phonebook book;
-
+    std::string cmd;
+    int count = 0;
     int i = 0;
+    int index;
 
-    while (i < 8)
+    while (1)
     {
-        std::cout << "add first name of contact " << i << " : ";
-        std::cin >> book.contact[i].f_name;
-        // book.contact[i].f_name = "ali\n" ;
-        i++;
+        std::cout << "Enter a command : ";
+        std::cin >> cmd;
+        if (cmd.compare("ADD") == 0)
+        {
+            
+            std::cout << "first name : ";
+            std::cin >> book.contact[i].f_name;
+
+            std::cout << "last name : ";
+            std::cin >> book.contact[i].l_name;
+
+            std::cout << "Nickname : ";
+            std::cin >> book.contact[i].nickname;
+
+            std::cout << "phone number : ";
+            std::cin >> book.contact[i].number;
+
+            std::cout << "Darkest secret : ";
+            std::cin >> book.contact[i].secret;
+            
+            std::cout << "contact added!" << std::endl;
+            i++;
+            count++;
+            if (i == 8)
+                i = 0;
+        }
+        else if (cmd.compare("SEARCH") == 0)
+        {
+            std::cout << "typed search\n";
+            if (count > 8)
+                count = 8;
+            book.list_contacts(count);
+            std::cout << "Enter an index : ";
+            std::cin >> index;
+            while ()
+
+        }
+        else if (cmd.compare("EXIT") == 0)
+            break;
     }
-    i = 0;
-    while (i < 8)
-    {
-        std::cout << book.contact[i].f_name << "\n";
-        i++;
-    }
+
+    std::cout << "The program exited\n";
 
 }
--- Module_00/ex01/Phonebook.hpp
+++ Module_00/ex01/Phonebook.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 
@@ -14,8 +15,28 @@ class Contact {
 class Phonebook {
     public:
         Contact contact[8];
-        void add_contact(int i, std::string name)
+        std::string turnc(std::string str)
         {
-            contact[i].f_name = name;
+            std::string str2 = str;
+            
+            if (str2.size() > 10)
+            {
+                str2 = str.substr(0, 9);
+                str2 += '.';     
+            }
+            return (str2);
+
+        }
+
+        void list_contacts(int end)
+        {
+            int i = 0;
+
+            std::cout << "|" << std::left << std::setw(10) << "index" << "|" << std::setw(10) << "first name" << "|" << std::setw(10) << "last Name" << "|" << std::endl;
+            while (i < end)
+            {
+                std::cout << "|" << std::left << std::setw(10) << i << "|" << std::setw(10) << turnc(contact[i].f_name) << "|" << std::setw(10) << turnc(contact[i].l_name) << "|" << std::endl;
+                i++;
+            }
         }
 };
