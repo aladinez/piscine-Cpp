e772e1d95fcda6382cd5679eb4dceee6cfbdfc81
aladinez <informaticienalae@gmail.com>
2021-12-14 13:23:55 +0100

    module00 is done.

--- Module_00/ex00/megaphone.cpp
+++ Module_00/ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aez-zaou <aez-zaou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aez-zaou <aez-zaou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 11:47:29 by aez-zaou          #+#    #+#             */
-/*   Updated: 2021/10/29 12:17:04 by aez-zaou         ###   ########.fr       */
+/*   Updated: 2021/12/14 13:23:06 by aez-zaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int main(int argc, char **argv)
             int j = 0;
             while (argv[i][j])
             {
+                // toupper returns an int.
                 std::cout << (char)toupper(argv[i][j]);
                 j++;
             }
